04041b247e841b56ebb7896ff3fcfe6842621e6b
bhavyagupta-11 <bhavyagupta740@gmail.com>
2023-12-18 08:03:01 +0530

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dotenv = require("dotenv");
 
 dotenv.config({path:"./config.env"});
